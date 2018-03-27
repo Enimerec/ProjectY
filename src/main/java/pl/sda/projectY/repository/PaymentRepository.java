3d976c42eb375b4621bac3988d9bedd661ff374e
MarczakMateusz <Mateusz.Szymon.Marczak@gmail.com>
2018-03-27 18:16:16 +0200

    bug fix v2 commit

--- src/main/java/pl/sda/projectY/repository/PaymentRepository.java
+++ src/main/java/pl/sda/projectY/repository/PaymentRepository.java
@@ -1,6 +1,8 @@
 package pl.sda.projectY.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import pl.sda.projectY.entity.Payment;
 
 
@@ -8,12 +10,15 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
-
+@Repository
+@Transactional
 public interface PaymentRepository extends JpaRepository<Payment,Integer> {
 
     List<Payment> findAll();
+    List<Payment>findAllByOrderByDateDesc();
 
     Payment findByPaymentId(int id);
 
-    List<Payment> findAllByStudent_userIdOrderByDate(int student);
+    List<Payment> findAllByStudent_UserIdOrderByDate(Integer student_userId);
+
 }
